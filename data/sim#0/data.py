16e23fb038d521571fa91252ccd456f7f8fa6311
hzlihang99 <hzlihang99@gmail.com>
2025-02-13 15:28:12 -0500

    Removed data/ and log/ folders from tracking

--- data/sim#0/data.py
+++
@@ -1,9 +0,0 @@
-import torch
-
-data = torch.load('data/sim#0/DynamicSim_uva_ic1_pathogen_aggH.pt')
-
-#Connvert the list of hypergraphs to a third dimension
-data.dynamic_hypergraph = torch.stack(data.dynamic_hypergraph, dim=0)
-print(data)
-
-torch.save(data, 'data/sim#0/DynamicSim_uva_ic1_pathogen_aggH.pt')
