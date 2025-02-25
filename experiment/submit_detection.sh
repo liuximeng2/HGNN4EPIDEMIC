89e6c903b7d2c7e70dc87afa86f7c1a115a7817a
liuximeng2 <sliu484@emory.edu>
2025-02-25 15:25:05 -0500

    added experiment submission scripts

--- experiment/submit_detection.sh
+++ experiment/submit_detection.sh
@@ -4,7 +4,7 @@
 datasets=("UVA" "EpiSim")
 
 # Define parameter combinations
-num_for_predict=(5 10 20)
+timestep_hidden=(5 10 20)
 location_aware_flags=(0 1)  # Binary flag (0=off, 1=on)
 hidden_channels=(128 256)
 lr=(0.005 0.001 0.0005)
@@ -18,15 +18,15 @@ for location_aware in "${location_aware_flags[@]}"; do
     else
         alpha=(1.0)
     fi
-    for num_for_predict in "${num_for_predict[@]}"; do
+    for timestep_hidden in "${timestep_hidden[@]}"; do
         for hc in "${hidden_channels[@]}"; do
             for l in "${lr[@]}"; do
                 for a in "${alpha[@]}"; do
                     for wd in "${weight_decay[@]}"; do
                         for ks in "${kernal_size[@]}"; do
-                            echo "Submitting job for dataset=$datasets, hidden_channels=$hc, lr=$l, alpha=$a, weight_decay=$wd, kernal_size=$ks, num_for_predict=$num_for_predict, location_aware=$location_aware"
-                            sbatch --export=datasets=$datasets,hidden_channels=$hc,lr=$l,alpha=$a,weight_decay=$wd,kernal_size=$ks,num_for_predict=$num_for_predict,location_aware=$location_aware experiment/run_forecast.sh
-                            # subsitude run_forecast.sh with your run file
+                            echo "Submitting job for dataset=$datasets, hidden_channels=$hc, lr=$l, alpha=$a, weight_decay=$wd, kernal_size=$ks, timestep_hidden=$timestep_hidden, location_aware=$location_aware"
+                            sbatch --export=hidden_channels=$hc,lr=$l,alpha=$a,weight_decay=$wd,kernal_size=$ks,timestep_hidden=$timestep_hidden,location_aware=$location_aware experiment/run.sh
+                            # subsitude run.sh with your run file
                         done
                     done
                 done
